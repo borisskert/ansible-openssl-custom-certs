c956a6c24329ca39092c92df2b2a276f71a42886
Boris Skert <boris.skert@gmail.com>
2020-04-13 11:47:34 +0200

    Create letsencrypte-like folder structure

--- tests/test.sh
+++ tests/test.sh
@@ -5,9 +5,9 @@ vagrant up --provision
 
 ansible-galaxy install -r requirements.yml -p ./roles --force
 
-ansible-playbook -i inventory.ini test.yml --extra-vars "force_cert_creation=false"
+ansible-playbook -i inventory.ini test.yml
 
-ansible-playbook -i inventory.ini test.yml --extra-vars "force_cert_creation=false" \
+ansible-playbook -i inventory.ini test.yml \
   | grep -q 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
